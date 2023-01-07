6ae7c065ccbfbf9b39e323be74e6798e4de5e024
lcarlos <luiscardozoicg@gmail.com>
2023-01-07 00:25:51 -0500

    request api and show data in html

--- js/app.js
+++ js/app.js
@@ -1,32 +1,41 @@
-const container = document.querySelector('.container');
-const resultado = document.querySelector('#resultado');
-const formulario = document.querySelector('#formulario');
+const container = document.querySelector(".container");
+const resultado = document.querySelector("#resultado");
+const formulario = document.querySelector("#formulario");
 
-window.addEventListener('load', () =>{
-    formulario.addEventListener('submit',buscarClima )
+window.addEventListener("load", () => {
+  formulario.addEventListener("submit", buscarClima);
 });
 
-function buscarClima(e){
-    e.preventDefault();
+function buscarClima(e) {
+  e.preventDefault();
 
-    const ciudad = document.querySelector('#ciudad').value;
-    const pais = document.querySelector('#pais').value;
+  const ciudad = document.querySelector("#ciudad").value;
+  const pais = document.querySelector("#pais").value;
 
-    if(ciudad === '' || pais === ''){
-        mostrarError('Ambos Campos son obligatorios');
-    
-    }
+  if (ciudad === "" || pais === "") {
+    mostrarError("Ambos Campos son obligatorios");
+    return;
+  }
+  consultarApi(ciudad, pais);
 }
 
+function mostrarError(mensaje) {
+  const alerta = document.querySelector(".bg-red-100");
 
-function mostrarError(mensaje){
-    const alerta = document.querySelector('.bg-red-100');
-
-     if(!alerta){
-
-    const alerta = document.createElement('div');
-    alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4',
-    'py-3', 'rounded','max-w-md', 'mx-auto', 'mt-6', 'text-center' );
+  if (!alerta) {
+    const alerta = document.createElement("div");
+    alerta.classList.add(
+      "bg-red-100",
+      "border-red-400",
+      "text-red-700",
+      "px-4",
+      "py-3",
+      "rounded",
+      "max-w-md",
+      "mx-auto",
+      "mt-6",
+      "text-center"
+    );
 
     alerta.innerHTML = `
         <strong class = "font-bold"> Error!</strong>
@@ -35,9 +44,45 @@ function mostrarError(mensaje){
     container.appendChild(alerta);
 
     setTimeout(() => {
-        alerta.remove();
-    }, 5000);    
+      alerta.remove();
+    }, 5000);
+  }
+}
 
-     }
+function consultarApi(ciudad, pais) {
+  const appId = "f257a171e195ae98dffdbda1552a411f";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}&lang=es`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((datos) => {
+      console.log(datos);
+      limpiarHTML();
+      if (datos.cod === "404") {
+        mostrarError("Ciudad no encontrada");
+        return;
+      }
+      mostrarClima(datos);
+    });
+}
 
+function mostrarClima(datos) {
+  const {
+    main: { temp, tem_max, temp_min },
+  } = datos;
+  const celcius = parseInt(temp - 273.15); 
+
+  const actual = document.createElement("p");
+  actual.innerHTML = `${celcius} &#8451`;
+  actual.classList.add("font-bold", "text-6xl");
+
+  const resultadoDiv = document.createElement("div");
+  resultadoDiv.classList.add("text-center", "text-white");
+  resultadoDiv.appendChild(actual);
+  resultado.appendChild(resultadoDiv);
+}
+
+function limpiarHTML(){
+  while(resultado.firstChild){
+    resultado.removeChild(resultado.firstChild);
+  }
 }
