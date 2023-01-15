43c71073bd0dcada1105220cfb85875c9082615e
lcarlos <luiscardozoicg@gmail.com>
2023-01-15 08:32:17 -0500

    quit attributs of map

--- js/app.js
+++ js/app.js
@@ -120,9 +120,7 @@ const createMapa = (lat, long) => {
   containerMap.appendChild(divmapa);
   const map = L.map("map").setView([lat, long], 6);
   L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
-    maxZoom: 19,
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    maxZoom: 19
   }).addTo(map);
 };
 
